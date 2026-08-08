26e4991ddbfc7beda474e607dc4344b464ee43e6
iharshilgupta <gupharshil007@gmail.com>
2026-08-08 09:18:02 +0530

    Add Calculating Function [A] from Codeforces

---
+++ 486/A - Calculating Function/solution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long long  a;
+    cin>>a;
+    if(a%2==0) cout<<a/2<<"
+";
+    else cout<<-((a+1)/2)<<"
+";
+    
+    return 0;
+    
+}
