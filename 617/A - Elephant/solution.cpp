6d890922dcdaf189129c25086cb99f65f9c9cc30
iharshilgupta <gupharshil007@gmail.com>
2026-08-08 09:17:30 +0530

    Add Elephant [A] from Codeforces

---
+++ 617/A - Elephant/solution.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //eleplant starts at 0
+    int a; // variable to take input 
+    cin>> a;
+    int sum=(a/5) + (a%5!=0);
+    cout<<sum; //dispalying sum
+    return 0;
+    
+}
